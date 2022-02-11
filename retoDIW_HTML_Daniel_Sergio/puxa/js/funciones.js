bb26a837966fc63ae368d9fe0a071dc3b7a71f58
YowiF <96142262+YowiF@users.noreply.github.com>
2022-02-11 13:38:14 +0100

    despliegue

---
+++ retoDIW_HTML_Daniel_Sergio/puxa/js/funciones.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $("#openNavegacion").click(function(){
+        $("#navegacion").css({"width":"250px"});
+        $(".imagenplato").hover(mostrartexto,ocultartexto);
+        $(".cajas").hover(handleInCajas,handleOutCajas);
+    $(".btn").hover(handleInBotones,handleOutBotones);
+    $("#celdaNav>a").hover(handleInEnlaces,handleOutEnlaces);
+    $("#celdaFooterPaginas>a").hover(handleInEnlaces,handleOutEnlaces);
+    });
+    
+    $(".closebtn").click(function(){
+        $("#navegacion").css({"width":"0px","borderLeft":"solid 2px black"});
+    });
+    
+    
+    function handleInCajas(){
+        $(this).css({
+            "background-color":"a7c1c0"
+        });
+    }
+    function handleOutCajas(){
+        $(this).css({
+            "background-color":"burlywood"
+            
+        });
+    }
+    function handleInBotones(){
+        $(this).css({
+            "background-color":"rgba(216,125,7,0.829)",
+            "cursor":"pointer"
+        });
+    }
+    function handleOutBotones(){
+        $(this).css({
+            "background-color":"#000"
+        });
+    }
+    function handleInEnlaces(){
+        $(this).css({
+           "color":"red"
+        });
+    }
+    function handleOutEnlaces(){
+        $(this).css({
+           "color":"rgba(255,255,255,0.76)"
+        });
+    }
+    
+    function mostrartexto(){
+        $(this).css({
+          opcaity:"0",
+      });
+      $(this).next().css({opacity:"1",});
+      }
+      function ocultartexto(){
+        $(this).css({
+          opcaity:"1",
+      });
+      var d=$(this).next().val();
+      $(this).next().css("opacity","0");
+      }
+        });
+    
