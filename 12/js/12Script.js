6e0b5932a789f83786cfccf588b0f8b4db27bc1b
Castro <37313157+CastroGamer88@users.noreply.github.com>
2022-02-20 12:50:57 +0100

    Cambios 12&12

---
+++ 12/js/12Script.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $(".imagenPlato").hover(mostrartexto, ocultartexto);
+    $(".cajas").hover(handleInCajas, handleOutCajas);
+    $(".btn").hover(handleInBotones, handleOutBotones);
+    $(".celdaNav>a").hover(handleInEnlaces, handleOutEnlaces);
+    $(".celdaFooterPaginas>a").hover(handleInEnlaces, handleOutEnlaces);
+  });
+  
+  function handleInCajas() {
+    $(this).css({
+      "background-color": "#a7c1c0",
+    });
+  }
+  function handleOutCajas() {
+    $(this).css({
+      "background-color": "burlywood",
+    });
+  }
+  function handleInBotones() {
+    $(this).css({
+      "background-color": "rgba(216,125,7,0.829)",
+      cursor: "pointer",
+    });
+  }
+  function handleOutBotones() {
+    $(this).css({
+      "background-color": "#000",
+    });
+  }
+  function handleInEnlaces() {
+    $(this).css({
+      color: "red",
+    });
+  }
+  function handleOutEnlaces() {
+    $(this).css({
+      color: "rgba(255,255,255,0.76)",
+    });
+  }
+  function mostrartexto() {
+    $(this).css({
+      opacity: "0",
+    });
+  
+    $(this).next().css({
+      opacity: "1",
+    });
+  }
+  
+  function ocultartexto() {
+    $(this).css({
+      opacity: "1",
+    });
+  
+    $(this).next().css({
+      opacity: "0",
+    });
+  }
+  
