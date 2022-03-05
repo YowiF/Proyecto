0ce59670553b75a939cc2b043b52b33d090696cc
Castro <37313157+CastroGamer88@users.noreply.github.com>
2022-03-05 20:15:19 +0100

    puxa casi lista

--- puxa/js/puxaScript.js
+++ puxa/js/puxaScript.js
@@ -73,3 +73,14 @@ $(".openNavegacion").click(function () {
     display = false;
   }
 });
+
+$("#pop").on("click", function () {
+  $("#imagepreview").attr("src", $("#map").attr("src")); // here asign the image to the modal when the user click the enlarge link
+  $("#imagemodal").modal("show"); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+$(function () {
+  $(".pop").on("click", function () {
+    $(".imagepreview").attr("src", $(this).find("img").attr("src"));
+    $("#imagemodal").modal("show");
+  });
+});
